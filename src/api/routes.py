7a9870cfdafb04fb9c7809923675379b63c6f0ea
Andres Longueira <andreslongueirarengifo@gmail.com>
2023-01-25 20:37:23 +0000

    Create token and endpoints

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,10 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, url_for, Blueprint, json
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token,jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,45 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def login_signup():
+    data = json.loads(request.data)
+    user = User.query.filter_by(name=data["name"], password=data["password"]).first()
+
+    if user is None:
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id }), 200
+
+
+@api.route('/login', methods=['POST'])
+def login_user():
+    data = json.loads(request.data)
+    user = User.query.filter_by(name=data["name"], password=data["password"]).first()
+
+    if user is None:
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id }), 200
+
+
+@api.route('/addUser', methods=['POST'])
+def new_user():
+    body = json.loads(request.data)
+    user_model = User(name = body["name"],password= body["password"])
+    db.session.add(user_model)
+    db.session.commit()
+    return jsonify("msj"),200
+
+@api.route('/user', methods=['GET'])
+def get_all_user():
+    users= User.query.all()
+    users_list = list(map(lambda obj: obj.serialize(), users))
+    response = {
+        "status": "ok",
+        "response": users_list
+    }
+    return jsonify(response)
